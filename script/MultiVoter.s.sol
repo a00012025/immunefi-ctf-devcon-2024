d07c91ae3005b60782b79f1f61ad879d21d74e87
Harry Chen <a00012025@gmail.com>
2024-11-23 21:56:11 +0800

    feat: multi voter

--- script/MultiVoter.s.sol
+++ script/MultiVoter.s.sol
@@ -12,64 +12,20 @@ contract MultiVoterScript is Script {
         address challengeAddr = address(
             0x4D86bB7fA5430Ace83b2886c6934e0d189c17027
         );
-        uint256 playerPrivateKey = uint256(
-            0xcdce3fb366787eed1632007a2e8cec8728fe8d461b474e979bfdada13954425a
-        );
+        address playerAddr = vm.addr(0x0123);
+
         Setup setup = Setup(challengeAddr);
         MultiVoterWallet wallet = setup.wallet();
 
-        console.log("wallet address: %s", address(wallet));
-
-        // Generate 6 addresses with known private keys
-        uint256[] memory privateKeys = new uint256[](6);
-        address[] memory voters = new address[](6);
-
-        uint idx = 18;
-        for (uint i = idx; i < idx + 6; i++) {
-            // Generate deterministic private keys
-            privateKeys[i - idx] = uint256(
-                keccak256(abi.encodePacked("voter", i))
-            );
-            voters[i - idx] = vm.addr(privateKeys[i - idx]);
-        }
-
-        (
-            address delegatee,
-            address owner,
-            bool registered,
-            uint256 voteCount,
-            uint256 validVotes
-        ) = wallet.voters(address(0xBe8A959533A4A91B57E8e981DE0ee0cFeE9f0364));
-        console.log("delegatee: %s", delegatee);
-        console.log("owner: %s", owner);
-        console.log("registered: %s", registered);
-        console.log("voteCount: %s", voteCount);
-        console.log("validVotes: %s", validVotes);
-
         vm.startBroadcast();
 
-        // Fund each address with 1 ETH
-        for (uint i = 0; i < voters.length; i++) {
-            payable(voters[i]).transfer(1 ether);
+        for (uint i = 0; i < 100; i++) {
+            wallet.delegate(address(0x1));
+            wallet.delegate(address(setup));
+            wallet.delegate(playerAddr);
         }
-
-        // Create proposal to call solve()
-        bytes memory data = abi.encodeCall(Setup.solve, ());
-        uint256 proposalId = wallet.propose(challengeAddr, data);
-        console.log("proposalId: %d", proposalId);
-        vm.stopBroadcast();
-
-        // Delegate votes to all generated addresses
-        for (uint i = 0; i < voters.length; i++) {
-            vm.startBroadcast(playerPrivateKey);
-            wallet.delegate(voters[i]);
-            vm.stopBroadcast();
-            vm.startBroadcast(privateKeys[i]);
-            wallet.vote();
-            vm.stopBroadcast();
-        }
-
-        vm.startBroadcast(playerPrivateKey);
+        wallet.propose(challengeAddr, abi.encodeWithSignature("solve()"));
+        wallet.vote();
         wallet.execute();
         vm.stopBroadcast();
 
--- test/MultiVoter.t.sol
+++ test/MultiVoter.t.sol
@@ -10,39 +10,22 @@ contract MultiVoterTest is Test {
     Setup public chal;
     MultiVoterWallet public wallet;
     address public player = makeAddr("player");
-    address public randomDelegatee = makeAddr("delegatee");
 
     function setUp() public {
-        console.log("player address: %s", player);
         chal = new Setup(player);
-        console.log("setup address: %s", address(chal));
         wallet = chal.wallet();
         vm.startPrank(player);
     }
 
     function testProposalAndDelegate() public {
-        // Create a proposal with random target and empty data
-
-        // Delegate votes to random address
         for (uint i = 0; i < 100; i++) {
-            wallet.delegate(randomDelegatee);
+            wallet.delegate(address(0x1));
             wallet.delegate(address(chal));
             wallet.delegate(player);
         }
-        address randomTarget = makeAddr("target");
-        wallet.propose(randomTarget, "");
-        // vm.stopPrank();
-        // vm.startPrank(randomDelegatee);
+        wallet.propose(address(chal), abi.encodeWithSignature("solve()"));
         wallet.vote();
-        // vm.stopPrank();
-
-        // vm.startPrank(player);
         wallet.execute();
-        // vm.stopPrank();
-
-        (bool finished, , , , , ) = wallet.proposals(0);
-        assertTrue(finished);
-
-        // wallet.delegate(player);
+        assert(chal.isSolved());
     }
 }
